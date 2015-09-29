5d029720536ac8a5e658930eaf0fdec56909e7e6
John Crawford <defcello@Johns-MacBook-Pro.local>
2015-09-29 02:11:36 -0400

    Organization changes.  Unit tests are working (YAY!).

--- _execommon.py
+++ _execommon.py
@@ -9,16 +9,36 @@ import sys
 
 
 
+DEFAULTCONFIGPATH = None
 ROOTDIR = Path(os.path.dirname(__file__))  #: "pathlib.Path" object to the root directory.
 
+def init(**kargs):
+    global DEFAULTCONFIGPATH
+    """
+    Initializes the project's execution environment.
+    :param kargs: Any number of keyword arguments with values to use for this
+    module's global constants.
+    """
+    if DEFAULTCONFIGPATH is None:
+        DEFAULTCONFIGPATH = kargs.get(
+            "DEFAULTCONFIGPATH",
+            ROOTDIR / 'user' / 'default' / 'cfg.json',
+        )
+    for path in (ROOTDIR, ROOTDIR / "lib" / "PyYAUL"):
+        path = str(path)
+        if path not in sys.path:
+            sys.path.append(path)
 
-
-def init():
-  """
-  Initializes the project's execution environment.
-  """
-  #Make sure root directory is in the system path.
-  for s in (ROOTDIR, ROOTDIR / "lib" / "pydefcello"):
-    s = str(s)
-    if s not in sys.path:
-      sys.path.append(s)
+def _initCfg(self, cfg):
+    """
+    Initializes the user configuration file.
+    :param cfg: `pathlib.Path` or `str` to user configuration file.  If it does
+      not exist, will create one by copying the default configuration file 
+    """
+    if not isinstance(cfg, dict):
+        global ROOTDIR
+        if not os.path.exists(cfg):
+            shutil.copyfile(DEFAULTCONFIGPATH, cfg)
+        with open(cfg) as fd:
+            cfg = json.load(fd)
+    self.cfg = cfg
--- runtests.py
+++ runtests.py
@@ -5,17 +5,16 @@ Run the unit tests by executing this script!
 
 import _execommon
 _execommon.init()  #Do this before anything else!
-
 from _execommon import ROOTDIR
-from defcello.execommon import showError
-from defcello.unittest import runTestsIn
+from pyyaul.base.execommon import showError
+from pyyaul.base.unittest import runTestsIn
 
 
 
 
 @showError
 def main():
-  runTestsIn(ROOTDIR, ROOTDIR)
+  runTestsIn(ROOTDIR)
 
 if __name__ == '__main__':
   main()
